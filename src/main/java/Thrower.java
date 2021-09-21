41f4a2d16ec704d8125ec326889d320832555fff
atenyaeves <atenyaev@protonmail.com>
2021-09-21 22:07:24 +0700

    Added readLine method

--- src/main/java/Thrower.java
+++ src/main/java/Thrower.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Class for throw exception
  */
@@ -7,5 +11,24 @@ public class Thrower {
      */
     public void throwException() throws Exception {
         throw new Exception();
+    }
+
+    /**
+     * Keyboard input
+     * @return str
+     */
+    public String readLine(){
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String string = "";
+        try {
+            string = reader.readLine();
+        }
+        catch (IOException e){
+            System.out.println("Checked");
+        }
+
+        return string;
     }
 }
