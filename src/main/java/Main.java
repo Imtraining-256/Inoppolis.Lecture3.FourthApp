a55479e6c8e6919c6404619d805ff1bd0fb39646
atenyaeves <atenyaev@protonmail.com>
2021-09-21 22:07:45 +0700

    Used readLine method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,10 +9,6 @@ public class Main {
     public static void main(String[] args) {
         Thrower thrower = new Thrower();
 
-        try {
-            thrower.throwException();
-        } catch (Exception e) {
-            System.out.println("Checked");
-        }
+        System.out.println(thrower.readLine());
     }
 }
