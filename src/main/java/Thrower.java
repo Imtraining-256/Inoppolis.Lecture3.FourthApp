5aba810562a950eb19a4f044b79e92534e59d869
atenyaeves <atenyaev@protonmail.com>
2021-09-21 21:46:14 +0700

    Added Thrower class

---
+++ src/main/java/Thrower.java
@@ -0,0 +1,11 @@
+/**
+ * Class for throw exception
+ */
+public class Thrower {
+    /**
+     * Method for exception
+     */
+    public void throwException() throws Exception {
+        throw new Exception();
+    }
+}
